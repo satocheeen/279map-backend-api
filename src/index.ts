1ec853360f2fca3a028ca7452c7117e84849bd18
Satoko <satoko@suzuki.email.ne.jp>
2023-12-22 12:06:30 +0900

    ビルドがうまくいかない問題の修正

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * as api from './api';
 export * as sns from './sns';
 export * from './types';
 export * from './auth';
+export * from './graphql/__generated__/types';
 
 export * from './utility';
 export * from '279map-common';
