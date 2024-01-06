289d9032517bb5231fe167833553c71ea46785b9
Satoko <satoko@suzuki.email.ne.jp>
2024-01-06 15:02:51 +0900

    common-typesをexport

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * as sns from './sns';
 export * from './types';
 export * from './auth';
 export * from './graphql/__generated__/types';
+export * from './types-common/common-types';
 
 export * from './utility';
