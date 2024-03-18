86620763bc4d2379d59b17554148633a94a8ada3
Satoko <satoko@suzuki.email.ne.jp>
2024-03-18 11:28:00 +0900

    ContentFieldDefineにradiusを追加

--- src/types-common/common-types.ts
+++ src/types-common/common-types.ts
@@ -92,7 +92,7 @@ export type DatasourceConfig = {
 
 export type ContentFieldDefine = {
     key: string;
-    type: 'title' | 'url' | 'latitude' | 'longitude' | 'address';
+    type: 'title' | 'url' | 'latitude' | 'longitude' | 'radius' | 'address';
 } | {
     key: string;
     type: 'date' | 'text' | 'category' | 'number' | 'image';
