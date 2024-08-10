79569dbc8689a18eaa0892cf4cd09dae624c6988
Satoko <satoko@suzuki.email.ne.jp>
2024-08-10 14:44:39 +0900

    ContentFieldDefine修正

--- src/types-common/common-types.ts
+++ src/types-common/common-types.ts
@@ -129,9 +129,14 @@ export type ContentFieldDefine = {
     key: string;
 
     // stringは１行、textは複数行. link=他アイテムへのリンク項目
-    type: 'title' | 'string' | 'date' | 'url' | 'text' | 'category' | 'single-category' | 'number' | 'image' | 'link';
+    type: 'title' | 'string' | 'date' | 'url' | 'text' | 'category' | 'single-category' | 'number' | 'image';
     label: string;
     readonly?: boolean;
+} | {
+    key: string;
+    type: 'link'
+    label: string;
+    databaseId: string;
 }
 
 /**
@@ -144,3 +149,22 @@ export type LocationFieldDefine = {
 
 
 export type ContentValueMapForDB = {[key: string]: any};
+export type ContentValueMap = {[key: string]: ContentValue};
+
+export type ContentValue = {
+    type: 'title' | 'string' | 'text' | 'date' | 'image' | 'url';
+    value: string;
+} | {
+    type: 'number';
+    value: number;
+} | {
+    type: 'category' | 'single-category';
+    value: string[];
+} | {
+    type: 'link';
+    value: {
+        dataId: DataId;     // リンク先アイテムID
+        name: string;       // リンク先アイテム名
+        mapKind: MapKind[]; // 当該アイテムが存在する地図種別
+    }[];
+}
