e10d75911e77fd1cb116ebf9b512e89fb7a34c88
Satoko <satoko@suzuki.email.ne.jp>
2024-09-12 18:31:11 +0900

    broadcastインタフェース見直し

--- src/api/broadcast.ts
+++ src/api/broadcast.ts
@@ -1,11 +1,21 @@
 import axios from 'axios';
-import { DataId } from '../types-common/common-types';
+import { DataId, MapKind } from '../types-common/common-types';
 
 export type BroadcastItemParam = {
-    mapId: string;
-    operation: 'insert' | 'update' | 'delete';
-    itemIdList: DataId[];
+    // 指定のデータに対して変更があった場合
+    operation: 'data-insert' | 'data-update';
+    targets: DataId[];
 } | {
+//     // 指定のデータが削除された場合 インタフェースが悩ましいので保留
+//     operation: 'data-delete';
+//     targets: {
+//         id: DataId;
+//         map: {
+//             id: string;
+//             kind: MapKind;
+//         }[];    // 複数の地図に属している場合もあるので
+//     }[];
+// } | {
     // 指定のデータソース定義に更新があった場合
     operation: 'datasource-define-update';
     datasourceId: string;
