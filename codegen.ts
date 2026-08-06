f67fe61499e4ab62ab6b93322aa18b260c92a8eb
Satoko <satoko@suzuki.email.ne.jp>
2026-08-06 12:03:04 +0900

    renameに伴う修正

--- codegen.ts
+++ codegen.ts
@@ -1,10 +1,10 @@
 
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
-// 279map-backend-main配下のtypes.tsを元に必要な型を生成する
+// backend-main配下のtypes.tsを元に必要な型を生成する
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../src/graphql/types.gql",
+  schema: "../backend-main/src/graphql/types.gql",
   generates: {
     "src/graphql/__generated__/types.ts": {
       plugins: [
