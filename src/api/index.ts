ef40f26cf391b2e6d6995c7c9bd31d5438f43cd9
Satoko <satoko@suzuki.email.ne.jp>
2026-08-09 09:16:50 +0900

    multer uninstasll

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,3 @@
 export { BroadcastItemParam, callBroadcast} from './broadcast';
-export { callOdbaApi, registFile } from '../odba/http';
+export { callOdbaApi } from '../odba/http';
 export * from '../contract';
